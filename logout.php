75b8e556877c97cb1330a3f89afbfe651bb97ce0
farfromunique <aaron@acwpd.com>
2013-07-09 16:41:56 -0700

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+// Availiable $_SESSION variables:
+//  CurrLoc: Integer tied to Locations Table UID
+//  CharUID: Integer tied to Character_Details UID
+//  CharName: String of the Characater Name
+//  IsGM: Boolean of whether or not this is a GM account
+//  LoggedIn: "Yes" or not "Yes" answer to the question "Is there a logged in person?"
+//  TZ: String containng a timezone
+
+session_start();
+session_destroy();
+header('Location: http://game.acwpd.com/');
+?>
