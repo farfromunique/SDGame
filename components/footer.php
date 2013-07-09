39f9c803bcd01802d2cb3134129a02e4c0a3ddfe
farfromunique <aaron@acwpd.com>
2013-07-09 16:45:40 -0700

    Create footer.php

---
+++ components/footer.php
@@ -0,0 +1,25 @@
+<?php
+// Availiable $_SESSION variables:
+//  CurrLoc: Integer tied to Locations Table UID
+//  CharUID: Integer tied to Character_Details UID
+//  CharName: String of the Characater Name
+//  IsGM: Boolean of whether or not this is a GM account
+//  LoggedIn: "Yes" or not "Yes" answer to the question "Is there a logged in person?"
+
+//footer file
+
+
+$LoginTimeStr = "UPDATE `Logins` SET `LastLogin`='" . $tu . "' WHERE `CharUID`='" . $_SESSION["CharUID"] . "'";
+if ($LoginTimeUpdate = mysqli_query($ud,$LoginTimeStr))
+{
+  // Everything worked!
+}
+else
+{
+	echo "<div id='Message' onclick='javascript:hideMessage();'>Not Updated<br />" . $LoginTimeStr . "</div>";
+}
+
+mysqli_close($con);
+mysqli_close($ud);
+echo "</body></html>";
+?>
