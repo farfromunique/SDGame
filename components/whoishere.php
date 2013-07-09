37e87bd84a89f44cdfd27693a94ba7568884d5f4
farfromunique <aaron@acwpd.com>
2013-07-09 16:51:00 -0700

    Create whoishere.php

---
+++ components/whoishere.php
@@ -0,0 +1,31 @@
+<?php
+// Availiable $_SESSION variables:
+//  CurrLoc: Integer tied to Locations Table UID
+//  CharUID: Integer tied to Character_Details UID
+//  CharName: String of the Characater Name
+//  IsGM: Boolean of whether or not this is a GM account
+//  LoggedIn: "Yes" or not "Yes" answer to the question "Is there a logged in person?"
+
+echo "<center>You are logged in as:<br />";
+  echo $_SESSION["CharName"];
+
+	echo "</center><br /><center>Others who are here:</center>
+	<hr />
+	<table width=100%>
+		<tr>
+			<th>Name</th>
+			<th width=25%>Rank</th>
+		</tr>";
+
+	while($row = mysqli_fetch_array($charsHere))
+	  {
+	  	if($row['UID']!=$currentChar)
+	  	{
+		  echo "<tr>";
+		  echo "<td><a href='show.php?id=" . $row['UID'] . "'>" . $row['Name'] . "</a></td>";
+		  echo "<td align='center'>" . $row['Rank'] . "</td>";
+		  echo "</tr>";
+	  	}
+	  }
+	echo "</table><hr /><center><a href='users.php'>All characters</a></center>";
+?>
