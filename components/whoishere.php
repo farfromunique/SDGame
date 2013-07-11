844c9f2c3b68baef046047691ef8c46ad108c998
farfromunique <aaron@acwpd.com>
2013-07-10 21:45:28 -0700

    whoishere.php: Bug #1 / #6

--- components/whoishere.php
+++ components/whoishere.php
@@ -1,37 +1,23 @@
-<?php
-// Availiable $_SESSION variables:
-//  CurrLoc: Integer tied to Locations Table UID
-//  CharUID: Integer tied to Character_Details UID
-//  CharName: String of the Characater Name
-//  IsGM: Boolean of whether or not this is a GM account
-//  LoggedIn: "Yes" or not "Yes" answer to the question "Is there a logged in person?"
-?>
 <div class='whoishere'>
-	<center>You are logged in as:<br />
-		<?php echo $_SESSION["CharName"]; ?>
-	</center><br />
-	<center>Others who are here:</center>
+	You are logged in as:<br />
+	<?php echo $_SESSION["CharName"]; ?>
+	<br />
+	Others who are here:
 	<hr />
 	<table width=100%>
 		<tr>
 			<th>Name</th>
 			<th width=25%>Rank</th>
 		</tr>
-<?php 
-	while($row = mysqli_fetch_array($charsHere))
-	  {
-	  	if($row['UID']!=$currentChar)
-	  	{
-		  echo "<tr>";
-		  echo "<td><a href='show.php?id=" . $row['UID'] . "'>" . $row['Name'] . "</a></td>";
-		  echo "<td align='center'>" . $row['Rank'] . "</td>";
-		  echo "</tr>";
-	  	}
-	  }
-?>
+		<?php while($row = mysqli_fetch_array($charsHere)): ?>
+			<?php if(!$row['UID']=$currentChar): ?>
+				<tr>
+					<td><a href='show.php?id=<?php echo $row['UID'] ?>'><?php echo $row['Name'] ?></a></td>
+					<td align='center'><?php echo $row['Rank'] ?></td>
+				</tr>
+			<?php endif; ?>
+		<?php endwhile; ?>
 	</table>
 	<hr />
-	<center>
-		<a href='users.php'>All characters</a>
-	</center>
+	<a href='users.php'>All characters</a>
 </div>
