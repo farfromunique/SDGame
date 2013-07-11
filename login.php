ba44237e4fb1027f789b6d61f83ac1025cf3e49c
farfromunique <aaron@acwpd.com>
2013-07-11 16:44:17 -0700

    login.php: revert #1 / #6
    Previous change results in error. Reverting, but also adding connections.php reference.

--- login.php
+++ login.php
@@ -9,6 +9,8 @@
 
 session_start();
 
+require 'components/connections.php';
+
 $UName = "'" . $_POST['User'] . "'";
 $PWord = md5($_POST['Pword']);
 
@@ -17,65 +19,69 @@ $loginStr="SELECT CharUID, Password, TimeZone FROM Logins WHERE LoginName = " . 
 $logins = mysqli_query($pw,$loginStr);
 
 $t=time();
-?>
 
-<?php if (mysqli_affected_rows($pw) == 1): ?>
-	<?php while ($row = mysqli_fetch_array($logins)): ?>
-		<?php if ($row['Password']==$PWord): ?>
-			<?php
+if (mysqli_affected_rows($pw) == 1) // Precisely 1 Login Name matches what was entered
+{
+	while ($row = mysqli_fetch_array($logins))
+		{
+			if ($row['Password']==$PWord) // Passwords Match
+			{
 				$_SESSION["LoggedIn"] = "Yes";
 				$_SESSION["CharUID"] = $row['CharUID'];
 				$_SESSION["TZ"]=$row['TimeZone'];
 				$LoggedInAt = mysqli_query($pw, "UPDATE Logins SET LastLogin " . $t . " WHERE UID = " . $_SESSION["CharUID"]);
 				$GetLoc = mysqli_query($pw,"SELECT Current_Location_W, Name FROM Character_Details WHERE UID = " . $_SESSION["CharUID"]);
-			?>	
-			<?php while ($row = mysqli_fetch_array($GetLoc)): ?>
-				<?php
-					$_SESSION["CurrLoc"] = $row['Current_Location_W'];
-					$_SESSION["CharName"] = $row['Name'];
-				?>
-			<?php endwhile; ?>
-			<?php $GM_Indicator = explode("-",$_SESSION["CharName"]); ?>
-			<?php if ($GM_Indicator[0] == "GM"): ?>
-				<?php $_SESSION["IsGM"] = True; ?>
-			<?php else: ?>
-				<?php $_SESSION["IsGM"] = False; ?>
-			<?php endif; ?>
-			<?php header('Location: http://game.acwpd.com/'); ?>
-			
-			
-		<?php else: ?>
-			Invalid Password. Try Again
-			<?php
+				while ($row = mysqli_fetch_array($GetLoc))
+			{
+				$_SESSION["CurrLoc"] = $row['Current_Location_W'];
+				$_SESSION["CharName"] = $row['Name'];
+			}
+			$GM_Indicator = explode("-",$_SESSION["CharName"]);
+			if ($GM_Indicator[0] == "GM")
+			{
+				$_SESSION["IsGM"] = True;
+			}
+			else
+			{
+				$_SESSION["IsGM"] = False;
+			}
+			header('Location: http://game.acwpd.com/');
+
+
+		}
+			else // Passwords don't match
+			{
+				echo "Invalid Password. Try Again";
 				$_SESSION["LoggedIn"] = "No";
 				$_SESSION["CharUID"] = 0;
-			?>
-			<html>
-			<head>
-			<link rel='stylesheet' type='text/css' href='basic.css'>
-			</head>
-			<?php require 'components/register.php'; ?>
-			</div>
-		<?php endif; ?>	
-	<?php endwhile; ?>
-<?php elseif (mysqli_affected_rows($pw) == 0): ?>
-	<?php
-		$_SESSION["LoggedIn"] = "No";
-		$_SESSION["CharUID"] = 0;
-	?>
-	<html>
-		<head>
-			<link rel='stylesheet' type='text/css' href='basic.css'>
-		</head>
-		<body>
-		<span>Invalid Login Name. Try Again.</span>  
-		<?php require 'components/register.php'; ?>
-<?php else: ?>
-	<?php
-		$message="Hey! Someone just managed to get more than 1 result on the query<br />" . $loginStr . "<br />Better check the DB!";
-		$headers = "From:TheGame@acwpd.com";
-		mail('game@acwpd.com','Duplicate Username',$message,$headers);
-	?>
-	Something very odd happened. The site admin has been contacted. Our apologies.
-<?php endif; ?>
-<?php require 'components/footer.php'; ?>
+				echo "<html>
+				<head>
+				<link rel='stylesheet' type='text/css' href='basic.css'>
+				</head>
+				<div id='Register'"; //Closing Angle Bracket missing intentionally
+				require 'components/register.php';
+				echo "</div>";
+			}	
+		}
+}
+else if (mysqli_affected_rows($pw) == 0) // Invalid Login Name
+{
+	$_SESSION["LoggedIn"] = "No";
+	$_SESSION["CharUID"] = 0;
+	echo "<html>
+	<head>
+	<link rel='stylesheet' type='text/css' href='basic.css'>
+	</head>
+	<body>";
+	echo "<span>Invalid Login Name. Try Again.</span";  //Closing Angle Bracket missing intentionally
+	require 'components/register.php';
+}
+else // Non-Unique LoginName (how did this happen?)
+{
+	$message="Hey! Someone just managed to get more than 1 result on the query<br />" . $loginStr . "<br />Better check the DB!";
+	$headers = "From:TheGame@acwpd.com";
+	mail('game@acwpd.com','Duplicate Username',$message,$headers);
+	echo "Something very odd happened. The site admin has been contacted. Our apologies.";
+}
+require 'components/footer.php';
+?>
