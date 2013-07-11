5e9dacaf5e0c318fe3ee752b58429671fe6ec786
farfromunique <aaron@acwpd.com>
2013-07-10 21:27:34 -0700

    ImportantStuff.php: Bug #1 / #6

--- components/ImportantStuff.php
+++ components/ImportantStuff.php
@@ -1,16 +1,5 @@
-<?php
-// Availiable $_SESSION variables:
-//  CurrLoc: Integer tied to Locations Table UID
-//  CharUID: Integer tied to Character_Details UID
-//  CharName: String of the Characater Name
-//  IsGM: Boolean of whether or not this is a GM account
-//  LoggedIn: "Yes" or not "Yes" answer to the question "Is there a logged in person?"
-//  TZ: String containng a timezone
-
-if ((!$_SESSION["TZ"]) and ($_SESSION["LoggedIn"]=="Yes"))
-{
-	echo "<div class='important' class='dismissable''>
+<?php if ((!$_SESSION["TZ"]) and ($_SESSION["LoggedIn"]=="Yes")): ?>
+	<div class='important' class='dismissable'>
 	Please take a moment to <a href='TZChoice.php'>set your timezone</a>. (<a href='javascript:DismissImportant();'>Dismiss Message</a>)
-	</div>";
-}
-?>
+	</div>
+<?php endif; ?>
