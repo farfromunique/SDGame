bf06375335f9379bc0f26f6fd3c389057451db10
farfromunique <aaron@acwpd.com>
2013-07-10 21:18:28 -0700

    converse.php: Bug #1 / #6
    Refactored using "alternative syntax" for control structures, to minimize actual processing needed, per #6 and #1

--- components/converse.php
+++ components/converse.php
@@ -7,8 +7,7 @@ if (isset($_REQUEST["UD_type"]))
 				$udstr="INSERT INTO Conversations (Location, TDS, Char_num, Comment)
 						VALUES ('" . $_SESSION["CurrLoc"] . "','$tu','$currentChar','$newComment')";
 				
-				$conversUD = mysqli_query($ud,$udstr);
-				
+				$conversUD = mysqli_query($ud,$udstr);	
 			}
 	}
 $conversStr = "SELECT Conversations.UID, Conversations.Location, Conversations.TDS, 
@@ -20,32 +19,31 @@ $convers = mysqli_query($con,$conversStr);
 $conversRows = mysqli_affected_rows($con);
 ?>
 <div class='conversation'>
-<?php
-if ($conversRows > 0)
-{
-	while ($row = mysqli_fetch_array($convers))
-		{
-			$TDS=strtotime($row['TDS']);
-			$TDS=$TDS-$tzOffset;
-			
-			echo "<div class='comment' id=" . $row['UID'] . ">";
-			if ($_SESSION["IsGM"])
-			{
-				echo "<form action='delete.php' method='POST' class='GM_Button'>
-						<input type='hidden' name='UID' value='" . $row['UID'] . "'>
-						<input type='hidden' name='Type' value='comment'>
-						<input type='submit' value='Delete'>
-					  </form>";
-			}
-			echo "<span class='attrib'><a href='show.php?id=" . $row['CharUID'] . "'>" . $row['Name'] .
-			"</a> wrote, at " . date('g:i A',$TDS) . ":</span>" . $row['Comment'] . "</div>";
-		}
-}
-else
-{
-	echo "<i>Nobody seems to have said anything for a while...</i>";
-}
-?>
+<?php if ($conversRows > 0): ?>
+	<?php while ($row = mysqli_fetch_array($convers)): ?>
+		<?php
+		$TDS=strtotime($row['TDS']);
+		$TDS=$TDS-$tzOffset;
+		?>
+		<div class='comment' id= <?php echo $row['UID']; ?> >
+			<?php if ($_SESSION["IsGM"]): ?>
+				<form action='delete.php' method='POST' class='GM_Button'>
+					<input type='hidden' name='UID' value=' <?php echo $row['UID']; ?> '>
+					<input type='hidden' name='Type' value='comment'>
+					<input type='submit' value='Delete'>
+				</form>
+			<?php endif; ?>
+			<span class='attrib'>
+				<a href='show.php?id=<?php echo $row['CharUID']; ?>'>
+					<?php echo $row['Name']; ?>
+				</a> wrote, at <?php echo date('g:i A',$TDS); ?>:
+			</span>
+			<?php echo $row['Comment']; ?>
+		</div>
+	<?php endwhile; ?>
+	<?php else: ?>
+		<i>Nobody seems to have said anything for a while...</i>
+	<?php endif; ?>
 	<form action='index.php' method='post'>
 	<input type='hidden' name='UD_type' value='Converse'>
 	<input type='text' name='Comm' class='TalkBox'>
