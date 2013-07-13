23954a886f08722adbae83c057fd4f849d35722f
Aaron C <aaron@acwpd.com>
2013-07-12 18:20:16 -0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,44 +1,27 @@
 <?php
-// Availiable $_SESSION variables:
-//  CurrLoc: Integer tied to Locations Table UID
-//  CharUID: Integer tied to Character_Details UID
-//  CharName: String of the Characater Name
-//  IsGM: Boolean of whether or not this is a GM account
-//  LoggedIn: "Yes" or not "Yes" answer to the question "Is there a logged in person?"
-//  TZ: String containng a timezone
-require 'components/header.php';
+session_start();
+if (!$_SESSION["IsGM"])
+{ header('Location: http://game.acwpd.com/'); }
+require 'components/connections.php';
 
-// Page-Specific variables
+$InsertType = $_POST["type"];
 
-// Page-Specific queries
-
-//Body
-?>
-<div class='content'>
-  <h2>Insert Into Database (GM-only)</h2>
-	<?php if (!$_SESSION["IsGM"]): ?>
-	You do not have permission to view this page.
+switch ($InsertType)
+{
+	case "Location":
+		$LocationW = $_POST["LocNameW"];
+		$LocationD = $_POST["LocNameD"];
+		$UpdateString = "INSERT INTO Locations (`LocationName_W`, `LocationName_D`) VALUES ($LocationW, $LocationD)";
+		break;
 	
-	<?php else: ?>
-	
-	What would you like to insert? <br />
-	<a href='#'>Locations</a> | <a href='#'>Powers</a>
+	default:
+		header('Location: http://game.acwpd.com/');
+		break;
+}
 
-	<div id='Location-Insert' class='hidden'>
-		<form action='insert2.php' method='post'>
-			Location name (waking)<input type='text' name='LocNameW'>
-			Location name (dreaming)<input type='text' name='LocNameD'>
-			<input type='hidden' name='type' value='Location'>
-			<input type='submit' value='Add!'>
-		</form>
-	</div>
-	
-	<div id='Power-Insert' class='hidden'>
-	
-	</div>
-<?php endif; ?>
-
-<?php
-//Footer
-require 'components/footer.php';
+if (!$Qry=mysqli_query($ud,$UpdateString))
+{
+	die('Error in query: ' . $UpdateString);
+}
+header('Location: http://game.acwpd.com/');
 ?>
