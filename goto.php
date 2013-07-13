6de99bb06d2aea79e913bdf0d749b30210c792bb
Aaron C <aaron@acwpd.com>
2013-07-12 18:34:48 -0700

    goto.php: Bug #12

--- goto.php
+++ goto.php
@@ -15,14 +15,22 @@ $targetLoc = $_REQUEST["Loc"];
 
 $currentLocationGetStr = "SELECT Current_Location_W FROM Character_Details WHERE UID = " . $_SESSION["CharUID"];
 $currentLocationSetStr = "UPDATE Character_Details SET Current_Location_W='" . $_REQUEST["Loc"] . "' WHERE UID = " . $_SESSION["CharUID"];
+$currentLocationName = "SELECT LocationName_W FROM Locations WHERE UID = " . $_REQUEST["Loc"];
 
 $SetLoc = mysqli_query($ud,$currentLocationSetStr);
 $GetLoc = mysqli_query($ud,$currentLocationGetStr);
+$GetName = mysqli_query($ud,$currentLocationName);
 
 while($row = mysqli_fetch_array($GetLoc))
 {
 	$_SESSION["CurrLoc"] = $row['Current_Location_W'];
 }
+
+while($row = mysqli_fetch_array($GetName))
+{
+	$_SESSION["CurrLocName"] = $row['LocationName_W'];
+}
+
 header('Location: http://game.acwpd.com/');
 require 'components/footer.php';
 ?>
