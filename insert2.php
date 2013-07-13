5ecd1fb014e122fdc6eda57b679530f69d9eaba9
Aaron C <aaron@acwpd.com>
2013-07-12 18:19:37 -0700

    Create insert2.php

---
+++ insert2.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if (!$_SESSION["IsGM"])
+{ header('Location: http://game.acwpd.com/'); }
+require 'components/connections.php';
+
+$InsertType = $_POST["type"];
+
+switch ($InsertType)
+{
+  case "Location":
+		$LocationW = $_POST["LocNameW"];
+		$LocationD = $_POST["LocNameD"];
+		$UpdateString = "INSERT INTO Locations (`LocationName_W`, `LocationName_D`) VALUES ($LocationW, $LocationD)";
+		break;
+	
+	default:
+		header('Location: http://game.acwpd.com/');
+		break;
+}
+
+if (!$Qry=mysqli_query($ud,$UpdateString))
+{
+	die('Error in query: ' . $UpdateString);
+}
+?>
