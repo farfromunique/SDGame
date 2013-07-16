3570efc096d595b8328fa52a362f9ebe2ccb98b0
Aaron C <aaron@acwpd.com>
2013-07-16 16:57:50 -0700

    Bug #20
    Fixing and Closing Bug #20

--- insert2.php
+++ insert2.php
@@ -11,19 +11,19 @@ switch ($InsertType)
 	case "Location":
 		$LocationW = '"' . $_POST["LocNameW"] . '"';
 		$LocationD = '"' . $_POST["LocNameD"] . '"';
-		$UpdateString = "INSERT INTO Locations (`LocationName_W`, `LocationName_D`) VALUES ($LocationW, $LocationD)";
+		$UpdateString = "INSERT INTO Locations (`LocationName_W`, `LocationName_D`) VALUES ($LocationW, $LocationD);";
 		break;
 		
 	case "Power":
-		$Power = '"' . $_POST["Power"] . '"';
-		$AddonType = '"' . $_POST["AddonType"] . '"';
-		$Name = '"' . $_POST["Name"] . '"';
-		$Description = '"' . $_POST["Description"] . '"';
-		$XP_Cost = '"' . $_POST["XP_Cost"] . '"';
-		$Requires_Specifics = '"' . $_POST["Requires_Specifics"] . '"';
-		$Requires_Other_Power = '"' . $_POST["Requires_Other_Power"] . '"';
+		$Power = "'" . $_POST["Power"] . "'";
+		$AddonType = "'" . $_POST["AddonType"] . "'";
+		$Name = "'" . $_POST["Name"] . "'";
+		$Description = $_POST["Description"];
+		$XP_Cost = "'" . $_POST["XP_Cost"] . "'";
+		$Requires_Specifics = "'" . $_POST["Requires_Specifics"] . "'";
+		$Requires_Other_Power = "'" . $_POST["Requires_Other_Power"] . "'";
 		$UpdateString = "INSERT INTO Powers (`Power`, `AddonType`, `Name`, `Description`, `XP_Cost`, `Requires_Specifics`, `Requires_Other_Power`)
-									 VALUES ($Power, $AddonType, $Name, $Description, $XP_Cost, $Requires_Specifics, $Requires_Other_Power)";
+									 VALUES ($Power, $AddonType, $Name, '$Description', $XP_Cost, $Requires_Specifics, $Requires_Other_Power);";
 		break;
 	
 	default:
@@ -33,7 +33,7 @@ switch ($InsertType)
 
 if (!$Qry=mysqli_query($ud,$UpdateString))
 {
-	die('Error in query: ' . $UpdateString);
+	die('Error in query: ' .  $UpdateString);
 }
-header('Location: http://game.acwpd.com/');
+header('Location: http://game.acwpd.com/insert.php');
 ?>
