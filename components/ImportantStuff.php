c8b01d5b669f73660465ec358076ff5400b052f7
Aaron C <aaron@acwpd.com>
2013-07-23 16:37:05 -0700

    ImportantStuff.php: #30

--- components/ImportantStuff.php
+++ components/ImportantStuff.php
@@ -1,5 +1,5 @@
 <?php if ((!$_SESSION["TZ"]) and ($_SESSION["LoggedIn"]=="Yes")): ?>
-	<div class='important' class='dismissable'>
+	<div class='important' id='Important'>
 	Please take a moment to <a href='TZChoice.php'>set your timezone</a>. (<a href='javascript:DismissImportant();'>Dismiss Message</a>)
 	</div>
 <?php endif; ?>
