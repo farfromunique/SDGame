626eef3bb8960b7e73920bd0b1ceda99c1c96af7
Aaron C <aaron@acwpd.com>
2013-07-31 19:44:20 -0700

    #39

--- index.php
+++ index.php
@@ -6,18 +6,86 @@
 //  IsGM: Boolean of whether or not this is a GM account
 //  LoggedIn: "Yes" or not "Yes" answer to the question "Is there a logged in person?"
 //  TZ: String containng a timezone
-require 'components/header.php';
+session_start();
 
-//Body
+#remove the directory path we don't want 
+$request  = str_replace("", "", $_SERVER['REQUEST_URI']); 
+#split the path by '/'  
+$site_array  = mb_split("/", $request);  
+
+foreach($site_array as $key => $value)
+{ 
+	if($value == "") { 
+		unset($site_array[$key]); 
+	} 
+} 
+
+$params = array_values($site_array);  
+
+switch ($params['0'])
+	{
+		case "kronjob":
+			$header = '';
+			$main = require_once ('kronjob.php');
+			$footer = '';
+			return;
+			
+		case "chat":
+			$header = require_once ('header.php');
+			$main = require_once ('converse.php');
+			$footer = require_once ('footer.php');
+			return;
+		
+		case "powers":
+			$header = require_once ('header.php');
+			$main = require_once ('buyPowers.php');
+			$footer = require_once ('footer.php');
+			return;
+			
+		case "character":
+			$header = require_once ('header.php');
+			$main = require_once ('newchar.php');
+			$footer = require_once ('footer.php');
+			return;
+			
+		case "goto":
+			$header = require_once ('header.php');
+			$main = require ('goto.php');
+			$footer = require_once ('footer.php');
+			return;
+			
+		case "timezone":
+			$header = require_once ('header.php');
+			$main = require_once ('TZChoice.php');
+			$footer = require_once ('footer.php');
+			return;
+			
+		case "insert":
+			$header = require_once ('header.php');
+			if ($_SESSION["IsGM"])
+			{ $main = require_once ('insert.php'); }
+			else
+			{ $main = require_once ('converse.php'); }
+			$footer = require_once ('footer.php');
+			return;
+			
+		default:
+			$header = require_once ('header.php');
+			$main = require_once ('converse.php');
+			$footer = require_once ('footer.php');
+			return;
+			
+	}
+echo $header;
 ?>
-<div class='content'>
-	<h2>You are currently at <?php echo $_SESSION["CurrLocName"]; ?></h2>
+
+<div class='headBar'>
+	<h2>You are currently at <span id='CurrLoc'><?php echo $_SESSION["CurrLocName"]; ?></span></h2>
 	<h3>It is now <?php echo $tf; ?></h3>
 </div>
-<?php
-//Conversation
-require 'components/converse.php';
-
-//Footer
-require 'components/footer.php';
-?>
+<div class='content' id='main'>
+<?php echo $main ?>
+</div>
+<!-- Footer Done -->
+<?php echo $footer ?>
+<!-- Footer Done -->
