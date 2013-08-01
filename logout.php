172eede0f68aa596545e2b7affce7fb69d0e3c58
Aaron C <aaron@acwpd.com>
2013-07-31 19:45:33 -0700

    #39

--- logout.php
+++ logout.php
@@ -11,7 +11,6 @@ session_start();
 session_destroy();
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-$extra = 'index.php';
-header("Location: http://$host$uri/$extra");
+header("Location: http://$host$uri/");
 exit;
 ?>
