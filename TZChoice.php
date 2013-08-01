b70a848215754a39df6dcc6cf7af6a216893a510
Aaron C <aaron@acwpd.com>
2013-07-31 19:35:37 -0700

    #39

--- TZChoice.php
+++ TZChoice.php
@@ -6,51 +6,43 @@
 //  IsGM: Boolean of whether or not this is a GM account
 //  LoggedIn: "Yes" or not "Yes" answer to the question "Is there a logged in person?"
 //  TZ: String containng a timezone
-
-require 'components/header.php';
+$d_root = './';
 
 //Body
 ?>
-<div id='content'><h2>Timezone List</h2>
-	<form action='tz_update.php' method='POST'>
-		<Table border='1'>
-			<tr>
-				<th width='45'>Select</th>
-				<th width='155'>Current Time</th>
-				<th>Timezone Name</th>
-				<th>Go To Submit</th>
-			</tr>
-			<?php
-$TZ_List=timezone_abbreviations_list();
-foreach($TZ_List as $x=>$x_value)
-  {  
-  foreach($TZ_List[$x] as $y=>$y_value)
-	{
-		echo "<tr>
-				<td align='center'><input type='radio' name='TZ' value='" . $y_value['timezone_id'] . "'></td>
-				<td align='center'>" . date('Y-m-d h:i A',time() + $y_value['offset']);
-				if ($y_value['dst']==1)
+<h2>Timezone List</h2>
+	<Table border='1'>
+		<tr>
+			<th width='45'>Select</th>
+			<th width='155'>Current Time</th>
+			<th>Timezone Name</th>
+			<th>Go To Submit</th>
+		</tr>
+		<?php
+			$TZ_List=timezone_abbreviations_list();
+			foreach($TZ_List as $x=>$x_value)
+			{  
+				foreach($TZ_List[$x] as $y=>$y_value)
 				{
-					echo "<br />**DST Active**";
+					?>
+					<tr>
+						<td align='center'><input type='radio' name='TZ' value='<?php echo $y_value['timezone_id']?>'></td>
+						<td align='center'><?php echo date('Y-m-d h:i A',time() + $y_value['offset']) ?>
+						<?php if ($y_value['dst']==1): ?>
+							<br />**DST Active**
+						<?php endif; ?>
+						</td>
+						<td align='center'><?php echo $y_value['timezone_id'] ?></td>
+						<td align-'center'><a href='#Foot'>Done?</a></td>
+					</tr>
+					<?php
 				}
-				echo "</td>
-				<td align='center'>" . $y_value['timezone_id'] . "</td>
-				<td align-'center'><a href='#Foot'>Done?</a></td>
-			</tr>";
-	}
-  	
-  }
-
-?>
-			<tr>
-				<td colspan='4' align='center'>
-					<a name='Foot'></a>
-					<input type='submit' value='Set Timezone'>
-				</td>
-			</tr>
-		</table>
-	</form>
-<?php
-//Footer
-require 'components/footer.php';
-?>
+			  }
+		?>
+		<tr>
+			<td colspan='4' align='center'>
+				<a name='Foot'></a>
+				<input type='submit' value='Set Timezone' id='tzset'>
+			</td>
+		</tr>
+	</table>
